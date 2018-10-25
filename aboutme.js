b63bb092f902e1e1a9eec04044956a1558229861
Ryna <pinktulips@Hotmail.com>
2018-10-25 08:52:52 -0700

    final lab 3

--- aboutme.js
+++ aboutme.js
@@ -11,9 +11,9 @@ if (responseOne === "Y" || responseOne === "YES") {
   alert("Nope. I was born in Burnsville, Washington.");
 } else {
   alert("That's right, I was not born in Seattle.");
-  numberCorrect++
+  numberCorrect++;
 }
-console.log ("numberCorrect", numberCorrect);
+console.log("numberCorrect", numberCorrect);
 console.log("This is the respone to question 1" + responseOne);
 
 //Second Question
@@ -24,9 +24,9 @@ if (responseTwo === "Y" || responseTwo === "YES") {
   alert("No way! So spciy!");
 } else {
   alert("You are correct, I do not like wasabi AT ALL");
-  numberCorrect++
+  numberCorrect++;
 }
-console.log ("numberCorrect", numberCorrect);
+console.log("numberCorrect", numberCorrect);
 console.log("This is the respone to question 2" + responseTwo);
 
 //Third Question
@@ -35,11 +35,11 @@ var responseThree = prompt("Do I have any children?").toUpperCase();
 //Conditional Statement
 if (responseThree === "Y" || responseThree === "YES") {
   alert("Correct! I have a son named Isaac.");
-  numberCorrect++
+  numberCorrect++;
 } else {
   alert("I actually have 1 child");
 }
-console.log ("numberCorrect", numberCorrect);
+console.log("numberCorrect", numberCorrect);
 console.log("This is the respone to question 3" + responseThree);
 
 //Foruth Question
@@ -48,11 +48,11 @@ var responseFour = prompt("Do I like to cook").toUpperCase();
 //Conditional Statement
 if (responseFour === "Y" || responseFour === "YES") {
   alert("You are correct! Although I am not very good it's fun!");
-  numberCorrect++
+  numberCorrect++;
 } else {
   alert("Of course I do, cooking is fun!");
 }
-console.log ("numberCorrect", numberCorrect);
+console.log("numberCorrect", numberCorrect);
 console.log("This is the respone to question 4" + responseFour);
 
 //Fifth Question
@@ -61,18 +61,18 @@ var responseFive = prompt("Do I like Autum season?").toUpperCase();
 ////Conditional Statement
 if (responseFive === "Y" || responseFive === "YES") {
   alert("Yes, It's my favorite season!");
-  numberCorrect++
+  numberCorrect++;
 } else {
   alert("I actually love Autum.");
 }
-console.log ("numberCorrect", numberCorrect);
+console.log("numberCorrect", numberCorrect);
 console.log("This is the respone to question 5" + responseFive);
 
 //Sixth Question
 
 //Conditional
 var siblings = 6;
-console.log('siblings', siblings);
+console.log("siblings", siblings);
 var guess = parseInt(prompt("How many siblings do I have?"));
 console.log("guess", guess);
 
@@ -94,44 +94,44 @@ while (guess !== siblings && i < 3) {
 //If right
 if (guess === 6) {
   alert("You're right! Congrat! I have " + siblings + " siblings!");
-  numberCorrect++
-}
-else { alert("Sorry, you are out of tries!");
-console.log ("numberCorrect", numberCorrect);
+  numberCorrect++;
+} else {
+  alert("Sorry, you are out of tries!");
+  console.log("numberCorrect", numberCorrect);
 }
 // Seventh Question
-var responseSeven = prompt("What are the names of all my siblings?").toUpperCase();
-var potentialResponse = ['Tony', 'David', 'Steve', 'Kevin', 'Justen', 'Lily'];
+prompt("What are the names of all my siblings?").toUpperCase();
+var responseSeven;
+var potentialResponse = ["Tony", "David", "Steve", "Kevin", "Justen", "Lily"];
 var guesses = 0;
 var answer = false;
 
-while (guesses < 5 && answer === false){
-
-for (var k = 0; k < potentialResponse.length; k++) {
-  if (responeSeven === potentialResponse[k]) {
-    answer = true;
+while (guesses < 5 && answer === false) {
+  for (var k = 0; k < potentialResponse.length; k++) {
+    if (responseSeven === potentialResponse[k]) {
+      answer = true;
+    }
   }
+  if (answer === false) {
+    responseSeven = prompt("Nope, try again.").toUpperCase();
+    console.log("responeSeven", responseSeven);
+  }
+  guesses++;
+}
+
+if (answer === true) {
+  alert("Congratulations! You guessed correctly!");
+  numberCorrect++;
 }
 if (answer === false) {
-  
-  responseSeven = prompt("Nope, try again.").toUpperCase();
-console.log("responeSeven", responeSeven);
+  alert("Sorry you were wrong.");
 }
-guesses++;
-}
-
-if (answer === true)
-alert("Congratulations! You guessed correctly!");
-numberCorrect++
-
-if (answer === false)
-alert("Sorry you were wrong.");
-
 
 //Score Card
 
-if (numberCorrect <= 4 ) {
+if (numberCorrect <= 4) {
   alert("You don't know me very well.");
- } else { alert("Wow you know me pretty well!");
- console.log ("numberCorrect", numberCorrect);
+} else {
+  alert("Wow you know me pretty well!");
+  console.log("numberCorrect", numberCorrect);
 }
